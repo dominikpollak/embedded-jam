052ad492cbbdfd60ad2f5e65550dd741fbed4fc6
David Choloyan <shamsani.david@gmail.com>
2024-11-09 10:28:06 +0100

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/esm",
+      format: "esm",
+      sourcemap: true,
+    },
+    plugins: [peerDepsExternal(), resolve({ extensions: [".js", ".jsx", ".ts", ".tsx"] }), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/umd",
+      format: "umd",
+      name: "JamLib",
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+      sourcemap: true,
+    },
+    plugins: [
+      peerDepsExternal(),
+      resolve({ extensions: [".js", ".jsx", ".ts", ".tsx"], browser: true }),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.d.ts",
+      format: "es",
+    },
+    plugins: [dts()],
+  },
+];
---
+++ src/components/Text.tsx
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+
+export const Text: FC = ({}) => {
+  return <span>Hello from your Lib!</span>;
+};
