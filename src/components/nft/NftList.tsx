83bee9d07ae5a6d8476b6cfbb443d6ccb6523def
dominikpollak <dominikpollak17@gmail.com>
2024-11-12 12:24:45 +0100

    feat:modal

--- src/components/nft/NftList.tsx
+++ src/components/nft/NftList.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import Modal from "../global/Modal";
 
-export const NftList: React.FC = () => {
-  return <span className="text-3xl text-red-500">NFT LIST</span>;
+interface Props {
+  text: string;
+  bg: string;
+}
+
+export const NftList: React.FC<Props> = ({ text, bg }: Props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <>
+      {isOpen && (
+        <Modal
+          onClose={() => setIsOpen(false)}
+          minWidth="400px"
+          minHeight="200px"
+        >
+          modal
+        </Modal>
+      )}
+      <span
+        onClick={() => setIsOpen(true)}
+        className="text-3xl"
+        style={{ color: text, backgroundColor: bg }}
+      >
+        NFT LIST
+      </span>
+    </>
+  );
 };
